54e9742822d9fdeaba41928546cfd75c3b0e8c2c
Javier <jtrochez.jate@gmail.com>
2018-08-21 13:04:20 -0500

    se medio agrega el boton para cambiar de pantalla-tambien se agrego una nueva pantalla

--- app/src/main/java/com/example/a1143878729/ejercicios4/MainActivity.java
+++ app/src/main/java/com/example/a1143878729/ejercicios4/MainActivity.java
@@ -1,13 +1,28 @@
 package com.example.a1143878729.ejercicios4;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button btnIrMenu;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnIrMenu = findViewById(R.id.btn_ir_menu);
+        btnIrMenu.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View view){
+                Intent intento = new Intent(getApplicationContext(), Menu.class);
+                nombreUsuario = elUserName.getText().toString();
+                
+            }
+        }
+
     }
 }
